371867d849685bf49f4699683bf2d7ccce09cad1
kimscastle <uiskim22@pos.idserve.net>
2024-01-23 15:53:29 +0900

    [DELETE] remove storyboard

---
+++ LevelUpiOS/LevelUpiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LevelUpiOS
+//
+//  Created by uiskim on 2024/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
