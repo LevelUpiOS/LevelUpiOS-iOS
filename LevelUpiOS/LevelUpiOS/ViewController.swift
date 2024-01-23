3337b5e26f6557971fb4cf3d60ad69ed605aebb1
kimscastle <uiskim22@pos.idserve.net>
2024-01-23 16:07:32 +0900

    [ADD] 테스트뷰컨추가

--- LevelUpiOS/LevelUpiOS/ViewController.swift
+++ LevelUpiOS/LevelUpiOS/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let testLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(.bold, ._30)
+        label.textColor = .designSystem(.white)
+        label.text = "테스트입니다"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .designSystem(.black)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(testLabel)
+        testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
 }
-
