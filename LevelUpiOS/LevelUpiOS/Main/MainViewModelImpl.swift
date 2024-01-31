1f492c8916f40ee288ed43bb5f29bbfd8453b145
MINJAE KIM <ffalswo2@naver.com>
2024-01-31 19:26:46 +0900

    [FEAT] MainViewModelImpl 구현 (#1)

--- LevelUpiOS/LevelUpiOS/Main/MainViewModelImpl.swift
+++ LevelUpiOS/LevelUpiOS/Main/MainViewModelImpl.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Combine
+
+
+final class MainViewModelImpl: MainViewModel {
+    func transform(input: MainViewModelInput) -> MainViewModelOutput {
+        let subject = input.viewWillAppear
+            .map { _ in
+                return Subject.dummy
+            }
+            .eraseToAnyPublisher()
+        
+        
+        return Output(topicsAndChapters: subject)
+    }
+    
+    
+}
