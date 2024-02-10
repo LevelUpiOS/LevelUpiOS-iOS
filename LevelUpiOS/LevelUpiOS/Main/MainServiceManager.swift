9252424d93c80ccc9a1f82fed187bbb65934e074
MINJAE KIM <ffalswo2@naver.com>
2024-02-10 14:47:09 +0900

    [FEAT] Category, Submission을 묶은 MainServiceManager 구현 (#11)

--- LevelUpiOS/LevelUpiOS/Main/MainServiceManager.swift
+++ LevelUpiOS/LevelUpiOS/Main/MainServiceManager.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+
+
+final class MainServiceManager {
+    
+    private let categoryService: CategoryService
+    
+    private let submissionService: SubmissionService
+    
+    init(categoryService: CategoryService, submissionService: SubmissionService) {
+        self.categoryService = categoryService
+        self.submissionService = submissionService
+    }
+    
+    func getChapters() async throws -> Subject {
+        do {
+            async let categories = try self.categoryService.getCategories()
+            async let submissions = self.submissionService.getSubmissions()
+            
+            let categoriesResult = try await categories
+            let submissionsResult = try await submissions
+            
+            return transformToAppData(
+                categories: categoriesResult.decodedData,
+                submissions: submissionsResult.decodedData
+            )
+        } catch {
+            guard let levelUpError = error as? LevelUpError else {
+                throw LevelUpError.unknownError
+            }
+            throw levelUpError
+        }
+    }
+    
+    private func transformToAppData(
+        categories: Categories,
+        submissions: SubmissionResponse) -> Subject {
+            var totalChapterCount = 0
+            let topics = categories.categories.map { category in
+                let topic = Topic(
+                    name: category.name,
+                    subname: category.description,
+                    chapters: category.exams.map { exam in
+                        totalChapterCount += 1
+                        var solveType: ChapterSolveType = .yet
+                        
+                        for submission in submissions.submissions {
+                            if submission.examId == exam.id {
+                                solveType = .solved(score: submission.score)
+                                break
+                            }
+                        }
+                        return Chapter(
+                            id: exam.id,
+                            name: exam.name,
+                            solveType: solveType
+                        )
+                    }
+                )
+                return topic
+            }
+            return Subject(
+                topics: topics,
+                totalCount: totalChapterCount,
+                solvedCount: submissions.submissions.count
+            )
+    }
+    
+    
+}
