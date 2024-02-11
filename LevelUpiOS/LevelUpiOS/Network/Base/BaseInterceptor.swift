238e7204076749f1578f7ade48347f1a75f1e6ea
kimscastle <uiskim22@pos.idserve.net>
2024-02-11 11:56:43 +0900

    [HOTFIX] urlrequest에 addvalue를 setvalue로 변경

--- LevelUpiOS/LevelUpiOS/Network/Base/BaseInterceptor.swift
+++ LevelUpiOS/LevelUpiOS/Network/Base/BaseInterceptor.swift
@@ -12,7 +12,7 @@ final class BaseInterceptor: RequestInterceptor {
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         var request = urlRequest
 
-        request.addValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.contentType.rawValue)
+        request.setValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.contentType.rawValue)
         request.addValue("X_AUTH_TOKEN=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3Iiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MDczNzQzMDN9.AcOnVzuM7GGom4WFrIW6fRGFt4xXHXTbsSbefemjZcY", forHTTPHeaderField: "cookie")
 
         completion(.success(request))
